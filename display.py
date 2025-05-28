5840557c0ea4994a78e446b40c2ac1aa1f7553ad
maksymfedunov <maksim.kherson88@gmail.com>
2025-05-28 21:24:28 +0200

    first_commit

---
+++ display.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Display(ABC):
+    
+    @abstractmethod
+    def display_message(self, message):
+        pass
+
+    @abstractmethod
+    def display_contacts(self, contacts):
+        pass
+
+    @abstractmethod
+    def display_birthdays(self, birthdays):
+        pass
+
+    @abstractmethod
+    def display_help(self):
+        pass
